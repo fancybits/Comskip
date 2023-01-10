0facbd8eaf24537fd6c0f7b8cee6b48f2f12aeab
Aman Karmani <aman@tmm1.net>
2023-01-09 16:18:00 -0800

    roll this back

--- platform.h
+++ platform.h
@@ -74,9 +74,7 @@ typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned __int64 uint64_t;
-#ifndef HARDWARE_DECODE
-#include <compat/w32pthreads.h>  // Is already defined in ffmpeg
-#endif
+#include "win32_pthread.h"
 
 #include <time.h>
 #else
